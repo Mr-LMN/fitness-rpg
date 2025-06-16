22dd332480f982235b378eef39719c0e047f7c00
Mr-LMN <lloydmartin94@gmail.com>
2025-06-16 23:03:31 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
-// Your project's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBI5P4HiwnMBZhQh5V2KOn-0_21-qRkDdc",
+  authDomain: "fitness-rpg-pencoedtre.firebaseapp.com",
+  projectId: "fitness-rpg-pencoedtre",
+  storageBucket: "fitness-rpg-pencoedtre.firebasestorage.app",
+  messagingSenderId: "255197443274",
+  appId: "1:255197443274:web:212a893767d6f55f25f033",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
+// Initialize Firebase services
 const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { db };
+export { db, auth, storage };
