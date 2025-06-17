8fb56a555ee389cf2f03e20c5a9edc9391c369b4
Mr-LMN <lloydmartin94@gmail.com>
2025-06-17 14:10:25 +0100

    Add inventory modal and backpack button (#67)

---
+++ src/components/InventoryModal.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./styles/InventoryModal.css";
+import "./styles/Overlay.css";
+
+function InventoryModal({ items = [], onUse, onClose }) {
+  return (
+    <div className="overlay-bg">
+      <div className="inventory-modal">
+        <h2>🎒 Inventory</h2>
+        {items.length === 0 && <p>No items found.</p>}
+        <ul>
+          {items.map((item) => (
+            <li key={item.id} className="inventory-item">
+              <div className="item-info">
+                <span className="item-name">{item.name}</span>
+                {item.type && (
+                  <span className="item-type">[{item.type}]</span>
+                )}
+                <p className="item-desc">{item.description}</p>
+              </div>
+              {!item.used && (
+                <button onClick={() => onUse && onUse(item.id)}>Use</button>
+              )}
+            </li>
+          ))}
+        </ul>
+        <button onClick={onClose}>Close</button>
+      </div>
+    </div>
+  );
+}
+
+export default InventoryModal;
